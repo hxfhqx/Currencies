d76c826d42cf47df262347f99ffbf76761be1c7b
hxfhqx <hxfhqx@163.com>
2018-01-10 21:37:12 +0800

    Add the launcher

--- app/src/main/java/com/hxf/currencies/SplashActivity.java
+++ app/src/main/java/com/hxf/currencies/SplashActivity.java
@@ -2,6 +2,7 @@ package com.hxf.currencies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class SplashActivity extends AppCompatActivity {
 
