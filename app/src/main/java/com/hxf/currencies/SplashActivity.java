8737d0e24d797d907511e975fa2759a95a634ab8
hxfhqx <hxfhqx@163.com>
2018-01-10 21:28:12 +0800

    Add the launcher

---
+++ app/src/main/java/com/hxf/currencies/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.hxf.currencies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+    }
+}
